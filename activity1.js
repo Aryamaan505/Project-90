c0137f69cc017437743d62d9fbb1220ced5a8390
Aryamaan505 <83063969+Aryamaan505@users.noreply.github.com>
2021-10-14 14:52:43 +0530

    Add files via upload

---
+++ activity1.js
@@ -0,0 +1,14 @@
+var score=0;
+
+function addscore(){
+    score=score+1;
+    document.getElementById("score").innerHTML=score; 
+}
+
+function addscore(){
+    localStorage.setItem("score",score); 
+}
+
+function addscore(){
+    window.location="activity_2.html";
+}
